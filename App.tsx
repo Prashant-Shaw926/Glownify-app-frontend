d7122321f8edc4771aaa88893a5667afcc79dccc
Prashant Shaw <prashant.shaw926@gmail.com>
2026-02-09 01:31:23 +0530

    Integrated NativeWind CSS and added custom theme configurations.

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import { StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AuthProvider } from './src/context/AuthContext';
 import RootNavigator from './src/navigation/RootNavigator';
+import "./global.css"
 
 export default function App() {
   return (
